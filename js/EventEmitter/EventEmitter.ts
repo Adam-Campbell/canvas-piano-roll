d1105b5fe4b92b02da1a18dacd08339e68b2c1ea
Adam <adamcampbelldev@gmail.com>
2020-02-17 21:59:34 +0000

    Added comments to EventEmitter and HistoryStack classes

--- js/EventEmitter/EventEmitter.ts
+++ js/EventEmitter/EventEmitter.ts
@@ -9,6 +9,10 @@ export default class EventEmitter {
 
     subscriptions: EventSubscription[] = [];
 
+    /**
+     * Creates a subscription that will call the given callback whenever the given event is fired.
+     * Returns a function that will end the subscription when called.
+     */
     subscribe = (event: Events, callback: Function) : Function => {
         this.subscriptions.push({ event, callback });
         return () => {
@@ -16,7 +20,12 @@ export default class EventEmitter {
         }
     }
 
-    emit = (event: Events, ...args: any[]) => {
+    /**
+     * If there are any current subscriptions for the event being fired it will call the callbacks
+     * associated with those subscriptions, passing them any additional arguments that were supplied
+     * to the emit method. 
+     */
+    emit = (event: Events, ...args: any[]) : void => {
         this.subscriptions.forEach(subscription => {
             if (event === subscription.event) {
                 subscription.callback(...args);
